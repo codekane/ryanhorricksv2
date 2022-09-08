2a221d97584039ecbcb19ef2c527379f07e447ae
Ryan Horricks <ryan.horricks@gmail.com>
2022-09-07 18:09:33 -0600

    Large implementation of first version of design... I'm considering making major changes to it, so I'm gonna commit now, and work on making this better.

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,12 +9,32 @@ import {
   ProjectsSection,
   Seo,
 } from "gatsby-theme-portfolio-minimal";
+import "../styles/homepage.css";
+
+function Hello(props) {
+  return (
+    <div className="welcome-header">
+      <div className="left">
+        <h1>Welcome!</h1>
+        <p>My name is Ryan Horricks, and I’m a Full-Stack Developer based out of Edmonton, Alberta with 5 years of experience.</p>
+        <p>Currently I’m working Part-Time as a Front End Developer with Advision eCommerce, in addition to working with clients as a Freelancer. I enjoy solving complex problems, making beautiful websites, building cool applications, and dreaming of better ways to live.</p>
+        <p>Besides code, I like to climb rocks, go to shows, play guitar, and I’m learning how to write for an audience.</p>
+      </div>
+      <div className="right">
+        <img src={"/profile.jpg"} />
+
+      </div>
+    </div>
+
+  )
+}
 
 export default function IndexPage() {
   return (
     <>
-      <Seo title="Gatsby Starter for Portfolio Minimal" />
-      <Page useSplashScreenAnimation>
+      <Seo title="Ryan Horricks" />
+      <Page>
+        <Hello />
         <HeroSection sectionId="hero" />
         <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />
         <AboutSection sectionId="about" heading="About Portfolio Minimal" />
