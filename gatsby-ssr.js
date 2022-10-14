8520368d50d373e7ae5a522368629708209e7b48
Ryan Horricks <ryan.horricks@gmail.com>
2022-10-13 18:49:56 -0600

    Hardcoding COLORS into the gatsby-ssr file to see if THAT will get it to work...

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,75 @@
 import React from 'react';
-import { COLORS } from './src/constants';
-
+//import { COLORS } from './src/constants';
+const COLORS = {
+  primaryColor: {
+    light: "#000",
+    dark: "#fafafa",
+  },
+  secondaryColor: {
+    light: "#fff4d9",
+    dark: "#341677",
+  },
+  tertiaryColor: {
+    light: "#f2f2f2",
+    dark: "#252525",
+  },
+  textColor: {
+    light: "#000",
+    dark: "rgba(255, 255, 255, 0.87)",
+  },
+  subtextColor: {
+    light: "#555",
+    dark: "#aaaaaa",
+  },
+  backgroundColor: {
+    light: "#fff",
+    dark: "#121212",
+  },
+  cardBackgroundColor: {
+    light: "#fff",
+    dark: "#1c1c1c",
+  },
+  scrollBarColor: {
+    light: "rgba(0, 0, 0, 0.5)",
+    dark: "rgba(255, 255, 255, 0.5)",
+  },
+  boxShadowColor: {
+    light: "rgba(0, 0, 0, 0.16",
+    dark: "rgba(0, 0, 0, 0.16",
+  },
+  boxShadowHoverColor: {
+    light: "rgba(0, 0, 0, 0.32)",
+    dark: "rgba(0, 0, 0, 0.32)",
+  },
+  codeBlockBg: {
+    light: "##f5f6f9",
+    dark: "#151f28",
+  },
+  codeBlockVar: {
+    light: "#da0078",
+    dark: "#ff39a7",
+  },
+  codeBlockNum: {
+    light: "#bf04b7",
+    dark: "#fed703",
+  },
+  codeBlockFun: {
+    light: "#3d5afe",
+    dark: "#c653ff",
+  },
+  codeBlockKeyword: {
+    light: "#fe4001",
+    dark: "#2bdcff",
+  },
+  codeBlockComment: {
+    light: "#7f7f7f",
+    dark: "#8292a2",
+  },
+  codeBlockTkimp: {
+    light: "#fd971f",
+    dark: "#fd971f",
+  }
+}
 
 function setColorsByTheme() {
   const processName = (name) => {
